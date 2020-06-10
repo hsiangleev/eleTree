4cf576e8349a883f802ebdce7625025600e6bdaa
hsiangleev <1547600955@qq.com>
2020-06-10 15:23:46 +0800

    文档

--- docsBuild/.vuepress/config.js
+++ docsBuild/.vuepress/config.js
@@ -33,6 +33,9 @@ module.exports = {
     markdown: {
         lineNumbers: true
     },
+    head: [
+        ['link', { rel: 'icon', type: 'image/png', href: '/favicon.ico' }]
+    ],
     configureWebpack: {
         resolve: {
             alias: {
