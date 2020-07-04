a7f73551fc73c6d81149310be34c92880cb45b1e
hsiangleev <1547600955@qq.com>
2020-07-04 17:27:59 +0800

    添加百度统计代码

--- docs/eleTree/js/baidu_count.js
+++
@@ -1,7 +0,0 @@
-var _hmt = _hmt || [];
-(function() {
-  var hm = document.createElement("script");
-  hm.src = "https://hm.baidu.com/hm.js?7fcfe1e92263ed7399914905523c0c95";
-  var s = document.getElementsByTagName("script")[0]; 
-  s.parentNode.insertBefore(hm, s);
-})();
--- docsBuild/.vuepress/config.js
+++ docsBuild/.vuepress/config.js
@@ -56,10 +56,13 @@ module.exports = {
         lineNumbers: true
     },
     head: [
+        ['meta', { name: 'baidu-site-verification', content: 'IvAUSITvsn' }],
+        ['meta', { name: 'description', content: 'eleTree2.0在线文档' }],
+        ['meta', { name: 'keywords', content: 'eleTree2.0在线文档' }],
         ['link', { rel: 'icon', type: 'image/png', href: '/favicon.ico' }],
         ['link', { rel: 'stylesheet', href: '/eleTree/css/icon.css' }],
         ['script', { src: '//eletreejs.surge.sh/eleTree.js?v=2.1.10.2' }],
-        ['script', { src: '/eleTree/js/baidu_count.js' }]
+        ['script', { }, 'var _hmt = _hmt || [];(function() {var hm = document.createElement("script");hm.src = "https://hm.baidu.com/hm.js?7fcfe1e92263ed7399914905523c0c95";var s = document.getElementsByTagName("script")[0]; s.parentNode.insertBefore(hm, s);})();']
     ],
     configureWebpack: {
         resolve: {
