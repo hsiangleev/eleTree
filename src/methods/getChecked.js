959460d1753fa819ee5c3d811e60aff3b6d49676
hsiangleev <1547600955@qq.com>
2021-06-18 10:30:47 +0800

    搜索之后选中节点时，过滤隐藏的节点

--- src/methods/getChecked.js
+++ src/methods/getChecked.js
@@ -1,23 +1,26 @@
 import getCurrentNodeData from '~/opera/getCurrentNodeData'
 import { paramDetection } from '~/opera/tools'
+import { symbolAttr } from '~/config'
 /**
  * 
  * @param {*是否只是叶子节点,默认值为 false} leafOnly 
  * @param {*是否包含半选节点,默认值为 false} includeHalfChecked 
+ * @param {*是否包含隐藏节点,默认值为 true,搜索时部分节点隐藏} includeHideChecked 
  */
-export default function(methods, leafOnly = false, includeHalfChecked = false) {
+export default function(methods, leafOnly = false, includeHalfChecked = false, includeHideChecked = true) {
     let options = this.config
     let {key, isOpen, checked, children, disabled, isLeaf} = options.request
     let results = []
 
     if(paramDetection(leafOnly, 'Boolean', 'getChecked方法第一个参数必须为Boolean')) return results
     if(paramDetection(includeHalfChecked, 'Boolean', 'getChecked方法第二个参数必须为Boolean')) return results
+    if(paramDetection(includeHideChecked, 'Boolean', 'getChecked方法第三个参数必须为Boolean')) return results
 
     let f=data=>{
         for(let i=0;i<data.length;i++){
             // 当状态为status，代表选中
             let fn = (status)=>{
-                if(data[i][checked] === status){
+                if(data[i][checked] === status && (includeHideChecked || !includeHideChecked && !data[i][symbolAttr.isHideNode])){
                     results.push(getCurrentNodeData.call(this, data[i]))
                 }
             }
--- src/methods/getRadioChecked.js
+++ src/methods/getRadioChecked.js
@@ -1,7 +1,10 @@
 import getCurrentNodeData from '~/opera/getCurrentNodeData'
 import { paramDetection } from '~/opera/tools'
 import { symbolAttr } from '~/config'
-export default function(methods) {
+/**
+ * @param {*是否包含隐藏节点,默认值为 true,搜索时部分节点隐藏} includeHideChecked 
+ */
+export default function(methods, includeHideChecked = true) {
     let options = this.config
     let {name, key, isOpen, checked, children, disabled, isLeaf, radioChecked} = options.request
 
@@ -13,7 +16,7 @@ export default function(methods) {
         let results = []
         let f=data=>{
             for(let i=0;i<data.length;i++){
-                if(data[i][radioChecked] === 2){
+                if(data[i][radioChecked] === 2 && (includeHideChecked || !includeHideChecked && !data[i][symbolAttr.isHideNode])){
                     results.push(getCurrentNodeData.call(this, data[i]))
                 }
                 if(data[i][children].length>0) f(data[i][children])
