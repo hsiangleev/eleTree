cb553e0abfc369a2414eb98e536696814789f979
李祥 <1547600955@qq.com>
2025-05-06 16:09:36 +0800

    Update AdvertiseTop.vue

--- docs/.vuepress/theme/components/AdvertiseTop.vue
+++ docs/.vuepress/theme/components/AdvertiseTop.vue
@@ -1,15 +1,6 @@
 <template>
     <!-- 头部广告位 -->
-    <div class="advertise-top">
-        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-        <ins class="adsbygoogle"
-            style="display:inline-block;width:210px;height:100px"
-            data-ad-client="ca-pub-2229355551485035"
-            data-ad-slot="6395441907"></ins>
-        <script>
-            (adsbygoogle = window.adsbygoogle || []).push({});
-        </script>
-    </div>
+    <div></div>
 </template>
 
 <script>
@@ -33,11 +24,3 @@ export default {
     }
 }
 </script>
-
-<style lang="stylus">
-.advertise-top
-    min-height: 102px;
-    padding: 1.5rem 1.5rem 0;
-    margin-bottom: -.5rem;
-    font-size: .75rem;
-</style>
